d6f3bff607a0d5452fbc286c286580e51ebbb62a
unknown <>
2023-12-06 20:51:51 -0300

    feat: detalhe

--- projetos/urls.py
+++ projetos/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from projetos.views import inicio,detalhe
 urlpatterns = [
     path("",inicio,name="inicio"),
-    path("detalhe",detalhe,name="detalhe")
+    path("<int:pk>/",detalhe,name="detalhe")
 ]
