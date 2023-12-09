37821cd09ddae57ed601f659e39d46893894081a
unknown <>
2023-12-08 21:09:33 -0300

    feat

--- projetos/views.py
+++ projetos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Projeto
+from rest_framework import viewsets
+from .serializers import ProjetoSerializer
 # Create your views here.
 def inicio(req) -> HttpResponse:
     projetos = Projeto.objects.all()
@@ -10,3 +12,6 @@ def detalhe(req,pk):
     projeto = Projeto.objects.filter(id=pk).first()
     contexto = {"projeto":projeto}
     return render(req,"projetos/detalhe.html",contexto)
+class ProjetoViewSet(viewsets.ModelViewSet):
+    queryset = Projeto.objects.all()
+    serializer_class = ProjetoSerializer
