421c699f8287e5b3cd905fc7efd09cc3b2698013
unknown <>
2023-12-08 21:15:05 -0300

    feat

--- projetos/urls.py
+++ projetos/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import path,include
 from projetos.views import inicio,detalhe
+from rest_framework import routers
+from projetos.views import ProjetoViewSet, detalhe, inicio
+
+router = routers.DefaultRouter()
+router.register(r"api",ProjetoViewSet)
+
 urlpatterns = [
     path("",inicio,name="inicio"),
-    path("<int:pk>/",detalhe,name="detalhe")
+    path("<int:pk>/",detalhe,name="detalhe"),
+    path("api-auth/",include("rest_framework.urls",namespace="rest_framework"))
 ]
+urlpatterns += router.urls
